158cca50c12d43a2f75a4631c230b0db8a84fb7e
Punith K <kpunith8@gmail.com>
2021-03-01 19:37:25 +0530

    Basic bot to welcome

---
+++ index.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+// Bot treats the strings with $ prefixed otherwise bot
+// will process all the messages matching the string
+const PREFIX = "$";
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("message", (msg) => {
+  const message = msg.content;
+  // Don't reply if bot sending the messages
+  if (msg.author.bot) return;
+
+  // Check for special prefix $ to reply
+  if (message.startsWith(PREFIX)) {
+    const [CMD_NAME, ...args] = message
+      .trim()
+      .substring(PREFIX.length)
+      .split(/\s+/);
+
+    console.log(CMD_NAME, args);
+
+    if (CMD_NAME === "welcome") {
+      msg.channel.send("Welcome to the channel :tada:");
+    }
+  }
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
